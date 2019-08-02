7a806f213d577a2824d762de51c3ee20be064dd9
Chentaotest <418251639@qq.com>
2019-08-02 11:23:22 +0800

    java

---
+++ spring_cloud_example-master/zeus_service_feign/src/main/java/com/example/controller/HiController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.service.SchedualService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Autor jiangcaijun
+ * @Date 2017/11/19
+ * @Time 21:02
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    private SchedualService schedualService;
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return "我是servie-feign：" + schedualService.sayHiFromClientOne(name);
+    }
+}
